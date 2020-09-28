c5632bff3d69a6eba3f9c291c9940fd271922d3b
Oscar Harrysson <harrysson.oscar@gmail.com>
2020-09-28 12:12:46 +0200

    kanske

--- myexpressjstask200928.js
+++ myexpressjstask200928.js
@@ -4,8 +4,8 @@ var PORT = process.env.PORT || 8080;
 var path = require('path');
 
 app.get('/', (req, res) => {
-    //res.sendFile(path.join(__dirname + 'public/home.html'));
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname + '/home.html'));
+    //res.send('Hello World!')
 })
 
 app.listen(PORT, () => {
