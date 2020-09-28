5fd122111ab7b6138da4b83f1d11a8ccbe9892dd
Oscar Harrysson <harrysson.oscar@gmail.com>
2020-09-28 12:10:31 +0200

    maybe

--- myexpressjstask200928.js
+++ myexpressjstask200928.js
@@ -4,8 +4,9 @@ var PORT = process.env.PORT || 8080;
 var path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + 'public/home.html'));
-});
+    //res.sendFile(path.join(__dirname + 'public/home.html'));
+    res.send('Hello World!')
+})
 
 app.listen(PORT, () => {
     console.log(`app listening at http://localhost:${PORT}`)
