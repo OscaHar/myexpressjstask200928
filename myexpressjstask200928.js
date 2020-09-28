22aaedfc1d264c5a06d47f4eb3fdf2783b78ec53
Oscar Harrysson <harrysson.oscar@gmail.com>
2020-09-28 13:33:51 +0200

    works locally, trying heroku

--- myexpressjstask200928.js
+++ myexpressjstask200928.js
@@ -5,8 +5,11 @@ var path = require('path');
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/home.html'));
+    res.sendFile(path.join(__dirname + '/about.html'));
     //res.send('Hello World!')
 })
+
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`app listening at http://localhost:${PORT}`)
