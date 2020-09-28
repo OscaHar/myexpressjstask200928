bf070208f2639123687217bfbb8b349a16a68c93
Oscar Harrysson <harrysson.oscar@gmail.com>
2020-09-28 11:18:49 +0200

    Added pages

--- myexpressjstask200928.js
+++ myexpressjstask200928.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express();
 const port = 8080
+var path = require('path');
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.sendFile(path.join(__dirname + 'public/home.html'));
+});
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`)
